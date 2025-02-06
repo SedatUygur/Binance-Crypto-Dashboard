a840a76494137f3eee80e000e02ca508d9735b9f
Sedat Uygur <sedat.uygur@outlook.com>
2025-02-06 14:18:58 +0300

    🎉 create apollo client

---
+++ client/src/lib/apolloClient.ts
@@ -0,0 +1,37 @@
+import { ApolloClient, InMemoryCache, HttpLink, split } from '@apollo/client';
+import { GraphQLWsLink } from '@apollo/client/link/subscriptions';
+import { createClient } from 'graphql-ws';
+import { getMainDefinition } from '@apollo/client/utilities';
+
+// HTTP link for queries and mutations
+const httpLink = new HttpLink({
+  uri: 'http://localhost:4000/', // my GraphQL endpoint
+});
+
+// WebSocket link for subscriptions using graphql-ws
+const wsLink = new GraphQLWsLink(
+  createClient({
+    url: 'ws://localhost:4000/subscriptions',
+    retryAttempts: 5,
+  })
+);
+
+// Using split to send data to each link depending on operation type
+const splitLink = split(
+  ({ query }) => {
+    const definition = getMainDefinition(query);
+    return (
+      definition.kind === 'OperationDefinition' &&
+      definition.operation === 'subscription'
+    );
+  },
+  wsLink,
+  httpLink
+);
+
+const client = new ApolloClient({
+  link: splitLink,
+  cache: new InMemoryCache(),
+});
+
+export default client;
