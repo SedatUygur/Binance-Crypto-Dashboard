3a3e5b5522fa2669afc1b2a1cc6f8b5e63126ef7
Sedat Uygur <sedat.uygur@outlook.com>
2025-02-06 16:00:05 +0300

    🎉 add the schema definition to the graphql service

--- services/graphql-service/index.ts
+++ services/graphql-service/index.ts
@@ -6,3 +6,30 @@ const redisSubscriber = new Redis({
   host: process.env.REDIS_HOST || 'localhost',
   port: process.env.REDIS_PORT || 6379,
 });
+
+// Schema Definition
+const typeDefs = gql`
+  type TradingPair {
+    symbol: String!
+    baseAsset: String
+    quoteAsset: String
+  }
+
+  type Ticker {
+    symbol: String!
+    price: String
+    high24hr: String
+    low24hr: String
+    priceChange24hr: String
+    priceChangePercent24hr: String
+  }
+
+  type Query {
+    tradingPairs: [TradingPair!]!
+    pairDetail(symbol: String!): Ticker
+  }
+
+  type Subscription {
+    tickerUpdate(symbol: String!): Ticker
+  }
+`;
