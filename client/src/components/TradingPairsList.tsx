2330107076dcc39305375c923a030d8baab2a95b
Sedat Uygur <sedat.uygur@outlook.com>
2025-02-08 15:09:33 +0300

    🎉 make trading pairs list responsive

--- client/src/components/TradingPairsList.tsx
+++ client/src/components/TradingPairsList.tsx
@@ -88,9 +88,9 @@ const TradingPairsList: React.FC<TradingPairsListProps> = ({ onSelectPair }) => 
         value={searchTerm}
         onChange={(e) => setSearchTerm(e.target.value)}
       />
-      <List>
+      <List className="flex flex-wrap justify-center">
         {filteredPairs.map((pair: TradingPair) => (
-          <ListItem key={pair.symbol}>
+          <ListItem key={pair.symbol} className="m-2 p-4 bg-gray-100 rounded-lg shadow-md w-full sm:w-1/2 md:w-1/4 lg:w-1/6">
             <PairButton onClick={() => onSelectPair(pair)}>
               {pair.symbol} ({pair.baseAsset}/{pair.quoteAsset})
             </PairButton>
